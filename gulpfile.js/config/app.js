b0f05aab3972f8bd00abb22fa0438f3eb9641132
a.kucheryavenkova <a.kucheryavenkova@m-games-ltd.com>
2022-02-11 10:14:02 +0300

    new ecran

--- gulpfile.js/config/app.js
+++ gulpfile.js/config/app.js
@@ -26,12 +26,16 @@ module.exports = {
             favicons: true,
             appleIcons: true,
             android: true,
-            window: true,
+            windows: true,
             yandex: false,
             coast: false,
             firefox: false,
-            appleStartup: false
+            appleStartup: false,
+            safari: true
         },
-        path: "/"
+        path: "./",
+        appName: 'My App',
+        appShortName: 'App',
+        appDescription: 'This is my application'
     }
 }
